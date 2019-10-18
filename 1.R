5ba2983a49a2b3715d07ccbcd97ef1d2e20496cd
Rahul Saxena <saxenism@gmail.com>
2019-10-19 04:09:25 +0530

    Added Tokenisation among others

--- 1.R
+++ 1.R
@@ -44,5 +44,61 @@ ggplot(spam.raw, aes(x = TextLength, fill = Label)) +
   labs(y = "Text Count", x = "Length of Text",
        title = "Distribution of Text Lengths with Class Labels")
 
+#we are to split our data into a training set and test set and we will use the caret package to make sure that it's a stratified split.
+library(caret)
 
+#Creating the standard 70%/30% stratified split!!
+set.seed(32984)
+indexes <- createDataPartition(spam.raw$Label, times = 1,
+                               p = 0.7, list = FALSE)
+
+train <- spam.raw[indexes,]
+test <- spam.raw[-indexes,]
+
+#Veifying the proportions of both new arrays..
+prop.table(table(train$Label))
+#      ham      spam 
+#0.8659318 0.1340682 
+prop.table(table(test$Label))
+#      ham      spam 
+#0.8659485 0.1340515 
+
+#Ohk, now we know Caret is actually pretty awesome!! Fuse me :P
+
+
+#Ok, now we would like to consider what is to be removed from our text(data pre-processing or data wrangling ya jo bhi hai!)
+#We would remove
+                #Casing
+                #Punctuations
+                #Numbers
+                #Stopwords and symbols
+                #And similar words (like running, run, ran, runs)
+
+
+#An example to see what happens before tokenisation and pre-processing
+train$Text[357]
+#[1] "Dear reached railway. What happen to you"
+
+library(quanteda)
+
+# Tokenize SMS text messages.
+train.tokens <- tokens(train$Text, what = "word", 
+                       remove_numbers = TRUE, remove_punct = TRUE,
+                       remove_symbols = TRUE, remove_hyphens = TRUE)
+
+
+#After tokenisation and pre-processing (Matlab thodi si hi!!)
+train.tokens[[357]]
+#[1] "Dear"    "reached" "railway" "What"    "happen"  "to"      "you" 
+
+train.tokens <- tokens_tolower(train.tokens)
+train.tokens[[357]]
+#[1] "dear"    "reached" "railway" "what"    "happen"  "to"      "you"   
+
+
+#Dekho Idhar mast saare stopwords nikaal diye!! R is just so damn awesome!!
+train.tokens <- tokens_select(train.tokens, stopwords(), 
+                              selection = "remove")
+
+stopwords()
 
